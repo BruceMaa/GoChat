5585e2484e8548fe15ec56bacecc2935ff3c9e8e
BruceMaa <bruce.maqiang@gmail.com>
2018-01-09 17:25:37 +0800

    添加微信公众号个性化菜单接口

--- wechat/mp/menu.go
+++ wechat/mp/menu.go
@@ -7,14 +7,18 @@ import (
 )
 
 const (
-	WechatMenuCreateApi = `https://api.weixin.qq.com/cgi-bin/menu/create?access_token=%s` // 自定义菜单创建接口
-	WechatMenuGetApi    = `https://api.weixin.qq.com/cgi-bin/menu/get?access_token=%s`    // 自定义菜单查询接口
-	WechatMenuDeleteApi = `https://api.weixin.qq.com/cgi-bin/menu/delete?access_token=%s` // 自定义菜单删除接口
+	WechatMenuCreateApi              = `https://api.weixin.qq.com/cgi-bin/menu/create?access_token=%s`         // 自定义菜单创建接口
+	WechatMenuGetApi                 = `https://api.weixin.qq.com/cgi-bin/menu/get?access_token=%s`            // 自定义菜单查询接口
+	WechatMenuDeleteApi              = `https://api.weixin.qq.com/cgi-bin/menu/delete?access_token=%s`         // 自定义菜单删除接口
+	WechatMenuConditionalAddApi      = `https://api.weixin.qq.com/cgi-bin/menu/addconditional?access_token=%s` // 创建个性化菜单
+	WechatMenuConditionalDeleteApi   = `https://api.weixin.qq.com/cgi-bin/menu/delconditional?access_token=%s` // 删除个性化菜单
+	WechatMenuConditionalTrymatchApi = `https://api.weixin.qq.com/cgi-bin/menu/trymatch?access_token=%s`       // 测试个性化菜单匹配结果
 )
 
 type (
 	Menu struct {
-		Menu Buttons `json:"menu"`
+		Menuid string  `json:"menuid,omitempty"`
+		Menu   Buttons `json:"menu,omitempty"`
 	}
 
 	Buttons struct {
@@ -30,6 +34,29 @@ type (
 		Pagepath  string   `json:"pagepath,omitempty"`   // 小程序的页面路径
 		SubButton []Button `json:"sub_button,omitempty"` // 二级菜单数组，个数应为1~5个
 	}
+
+	Conditional struct {
+		Buttons
+		Matchrule ConditionalRule `json:"matchrule"`
+	}
+
+	ConditionalRule struct {
+		TagId              string        `json:"tag_id,omitempty"`                      // 用户标签的id，可通过用户标签管理接口获取
+		Sex                WechatUserSex `json:"sex,string,omitempty"`                  // 性别：男（1）女（2），不填则不做匹配
+		Country            string        `json:"country,omitempty"`                     // 国家信息，是用户在微信中设置的地区，具体请参考地区信息表
+		Province           string        `json:"province,omitempty"`                    // 省份信息，是用户在微信中设置的地区，具体请参考地区信息表
+		City               string        `json:"city,omitempty"`                        // 城市信息，是用户在微信中设置的地区，具体请参考地区信息表
+		Language           string        `json:"language,omitempty"`                    // 语言信息，是用户在微信中设置的语言，具体请参考语言表： 1、简体中文 "zh_CN" 2、繁体中文TW "zh_TW" 3、繁体中文HK "zh_HK" 4、英文 "en" 5、印尼 "id" 6、马来 "ms" 7、西班牙 "es" 8、韩国 "ko" 9、意大利 "it" 10、日本 "ja" 11、波兰 "pl" 12、葡萄牙 "pt" 13、俄国 "ru" 14、泰文 "th" 15、越南 "vi" 16、阿拉伯语 "ar" 17、北印度 "hi" 18、希伯来 "he" 19、土耳其 "tr" 20、德语 "de" 21、法语 "fr"
+		ClientPlatformType string        `json:"client_platform_type,string,omitempty"` // 客户端版本，当前只具体到系统型号：IOS(1), Android(2),Others(3)，不填则不做匹配
+	}
+)
+
+type WechatClientPlatformType int
+
+const (
+	WechatClientPlatformTypeIOS     WechatClientPlatformType = iota + 1 // iOS 系统
+	WechatClientPlatformTypeAndroid                                     // Android 系统
+	WechatClientPlatformTypeOthers                                      // 其他手机系统
 )
 
 // 自定义菜单创建接口
@@ -40,8 +67,7 @@ func (wm *WechatMp) CreateMenu(accessToken string, buttons Buttons) (*common.Pub
 		return nil, fmt.Errorf("menu create error: %+v\n", err)
 	}
 	var result common.PublicError
-	err = json.Unmarshal([]byte(resp), &result)
-	if err != nil {
+	if err = json.Unmarshal([]byte(resp), &result); err != nil {
 		fmt.Fprintf(common.WechatErrorLoggerWriter, "menu create error: %+v\n", err)
 		return nil, fmt.Errorf("menu create error: %+v\n", err)
 	}
@@ -56,8 +82,7 @@ func (wm *WechatMp) GetMenu(accessToken string) (*Menu, error) {
 		return nil, fmt.Errorf("menu get error: %+v\n", err)
 	}
 	var menu Menu
-	err = json.Unmarshal([]byte(resp), &menu)
-	if err != nil {
+	if err = json.Unmarshal([]byte(resp), &menu); err != nil {
 		fmt.Fprintf(common.WechatErrorLoggerWriter, "menu get error: %+v\n", err)
 		return nil, fmt.Errorf("menu get error: %+v\n", err)
 	}
@@ -72,12 +97,61 @@ func (wm *WechatMp) DeleteMenu(accessToken string) (*common.PublicError, error) 
 		return nil, fmt.Errorf("menu delete error: %+v\n", err)
 	}
 	var result common.PublicError
-	err = json.Unmarshal([]byte(resp), &result)
-	if err != nil {
+	if err = json.Unmarshal([]byte(resp), &result); err != nil {
 		fmt.Fprintf(common.WechatErrorLoggerWriter, "menu delete error: %+v\n", err)
 		return nil, fmt.Errorf("menu delete error: %+v\n", err)
 	}
 	return &result, nil
 }
 
-// TODO 自定义菜单事件推送
+/* 自定义菜单事件推送 */
+
+// 创建个性化菜单
+func (wm *WechatMp) AddConditional(accessToken string, conditional Conditional) (string, error) {
+	resp, err := common.HTTPPostJson(fmt.Sprintf(WechatMenuConditionalAddApi, accessToken), conditional)
+	if err != nil {
+		fmt.Fprintf(common.WechatErrorLoggerWriter, "menu conditional add error: %+v\n", err)
+		return "", fmt.Errorf("menu conditional add error: %+v\n", err)
+	}
+	var menu Menu
+	if err = json.Unmarshal([]byte(resp), &menu); err != nil {
+		fmt.Fprintf(common.WechatErrorLoggerWriter, "menu conditional add error: %+v\n", err)
+		return "", fmt.Errorf("menu conditional add error: %+v\n", err)
+	}
+	return menu.Menuid, nil
+}
+
+// 删除个性化菜单
+func (wm *WechatMp) DeleteConditional(accessToken string, menuid string) (*common.PublicError, error) {
+	menu := &Menu{
+		Menuid: menuid,
+	}
+	resp, err := common.HTTPPostJson(fmt.Sprintf(WechatMenuConditionalDeleteApi, accessToken), menu)
+	if err != nil {
+		fmt.Fprintf(common.WechatErrorLoggerWriter, "menu conditional delete error: %+v\n", err)
+		return nil, fmt.Errorf("menu conditional delete error: %+v\n", err)
+	}
+	var result common.PublicError
+	if err = json.Unmarshal([]byte(resp), &result); err != nil {
+		fmt.Fprintf(common.WechatErrorLoggerWriter, "menu conditional delete error: %+v\n", err)
+		return nil, fmt.Errorf("menu conditional delete error: %+v\n", err)
+	}
+	return &result, nil
+}
+
+// 测试个性化菜单匹配结果
+func (wm *WechatMp) TrymatchConditional(accessToken, userId string) (*Buttons, error) {
+	var args = make(map[string]string)
+	args["user_id"] = userId
+	resp, err := common.HTTPPostJson(fmt.Sprintf(WechatMenuConditionalTrymatchApi, accessToken), args)
+	if err != nil {
+		fmt.Fprintf(common.WechatErrorLoggerWriter, "menu conditional trymatch error: %+v\n", err)
+		return nil, err
+	}
+	var buttons Buttons
+	if err = json.Unmarshal([]byte(resp), buttons); err != nil {
+		fmt.Fprintf(common.WechatErrorLoggerWriter, "menu conditional trymatch error: %+v\n", err)
+		return nil, err
+	}
+	return &buttons, nil
+}
--- wechat/mp/user.go
+++ wechat/mp/user.go
@@ -27,30 +27,30 @@ const (
 	WechatUserGetApi = `https://api.weixin.qq.com/cgi-bin/user/get?access_token=%s&next_openid=%s` // 获取用户列表
 )
 
-type WechatSex int
+type WechatUserSex int
 
 const (
-	WechatSexUnknow WechatSex = iota // 未知性别
-	WechatSexMale                    // 男性
-	WechatSexFemale                  // 女性
+	WechatSexUnknow WechatUserSex = iota // 未知性别
+	WechatSexMale                        // 男性
+	WechatSexFemale                      // 女性
 )
 
 type (
 	WechatUserInfo struct {
-		Subscribe     int       `json:"subscribe"`      // 用户是否订阅该公众号标识，值为0时，代表此用户没有关注该公众号，拉取不到其余信息。
-		Openid        string    `json:"openid"`         // 用户的标识，对当前公众号唯一
-		Nickname      string    `json:"nickname"`       // 用户的昵称
-		Sex           WechatSex `json:"sex"`            // 用户的性别，值为1时是男性，值为2时是女性，值为0时是未知
-		City          string    `json:"city"`           // 用户所在城市
-		Country       string    `json:"country"`        // 用户所在国家
-		Province      string    `json:"province"`       // 用户所在省份
-		Language      string    `json:"language"`       // 用户的语言，简体中文为zh_CN
-		Headimgurl    string    `json:"headimgurl"`     // 用户头像，最后一个数值代表正方形头像大小（有0、46、64、96、132数值可选，0代表640*640正方形头像），用户没有头像时该项为空。若用户更换头像，原有头像URL将失效。
-		SubscribeTime int64     `json:"subscribe_time"` // 用户关注时间，为时间戳。如果用户曾多次关注，则取最后关注时间
-		Unionid       string    `json:"unionid"`        // 只有在用户将公众号绑定到微信开放平台帐号后，才会出现该字段。
-		Remark        string    `json:"remark"`         // 公众号运营者对粉丝的备注，公众号运营者可在微信公众平台用户管理界面对粉丝添加备注
-		Groupid       int       `json:"groupid"`        // 用户所在的分组ID（兼容旧的用户分组接口）
-		TagidList     []int     `json:"tagid_list"`     // 用户被打上的标签ID列表
+		Subscribe     int           `json:"subscribe"`      // 用户是否订阅该公众号标识，值为0时，代表此用户没有关注该公众号，拉取不到其余信息。
+		Openid        string        `json:"openid"`         // 用户的标识，对当前公众号唯一
+		Nickname      string        `json:"nickname"`       // 用户的昵称
+		Sex           WechatUserSex `json:"sex"`            // 用户的性别，值为1时是男性，值为2时是女性，值为0时是未知
+		City          string        `json:"city"`           // 用户所在城市
+		Country       string        `json:"country"`        // 用户所在国家
+		Province      string        `json:"province"`       // 用户所在省份
+		Language      string        `json:"language"`       // 用户的语言，简体中文为zh_CN
+		Headimgurl    string        `json:"headimgurl"`     // 用户头像，最后一个数值代表正方形头像大小（有0、46、64、96、132数值可选，0代表640*640正方形头像），用户没有头像时该项为空。若用户更换头像，原有头像URL将失效。
+		SubscribeTime int64         `json:"subscribe_time"` // 用户关注时间，为时间戳。如果用户曾多次关注，则取最后关注时间
+		Unionid       string        `json:"unionid"`        // 只有在用户将公众号绑定到微信开放平台帐号后，才会出现该字段。
+		Remark        string        `json:"remark"`         // 公众号运营者对粉丝的备注，公众号运营者可在微信公众平台用户管理界面对粉丝添加备注
+		Groupid       int           `json:"groupid"`        // 用户所在的分组ID（兼容旧的用户分组接口）
+		TagidList     []int         `json:"tagid_list"`     // 用户被打上的标签ID列表
 		common.PublicError
 	}
 
